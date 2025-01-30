0fe09ab6b1c567e6ba6c8fdbf860c277935eedf3
Gaurav Bhatti <dw1227@wayne.edu>
2025-01-30 12:06:28 -0500

    Update cca definition code to prepare files for review by specialist, addresses #1

--- code/assign_samples_cca_filter_ptb_control.R
+++ code/assign_samples_cca_filter_ptb_control.R
@@ -22,7 +22,11 @@ ano_387$cca<-ifelse(apply(ano_387[, c("PH_G1_ST11",
                                                  "PH_G2_ST12",
                                                  "PH_G3_ST21",
                                                  "PH_G4_ST22",
-                                                 "PH_G5_CPI")],1,myf0)>=1,1,0) 
+                                                 "PH_G5_CPI")],1,myf0)>=1,1,0)
+
+
+write_csv(ano_387,file="data/sample_annotation/cfrna387/cfRNA_387_Transcriptomics_ano.csv")
+
 
 ano_387<-ano_387 |> 
   filter(Group %in% c("Labor","NoLabor","PTL","PPROM")) |> 
@@ -41,6 +45,9 @@ ano_crna_dream$cca<-ifelse(apply(ano_crna_dream[, c("PH_G1_ST11",
                                       "PH_G3_ST21",
                                       "PH_G4_ST22",
                                       "PH_G5_CPI")],1,myf0)>=1,1,0) 
+write_csv(ano_crna_dream,file="data/sample_annotation/crnadream/DREAM_Transcriptomics_ano.csv")
+
+
 
 ano_crna_dream<-ano_crna_dream |> 
   mutate(Group2= if_else(Group %in% c("Control"),"control","ptb"))
